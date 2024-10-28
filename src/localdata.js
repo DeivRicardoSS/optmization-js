6e7b32bd07f68fc0cb4aea259ad02cdfbafb06db
Deivyson <131066052+DeivRicardoSS@users.noreply.github.com>
2024-10-28 15:49:36 -0300

    update

--- src/localdata.js
+++ src/localdata.js
@@ -5,3 +5,11 @@ export function setData(key, value) {
 export function getData(key) {
     return JSON.parse(localStorage.getItem(key));
 }
+
+export function updateData(key, key2, valor) {
+    let obj = getData(key);
+    if(key2 in obj) {
+        obj[key2] = valor
+        setData(key, obj);
+    }
+}
